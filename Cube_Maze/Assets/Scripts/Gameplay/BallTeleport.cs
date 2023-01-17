5df795f6137fc2291c1b10cb20bd64e84de11c8d
Alexey <balabolinad@gmail.com>
2023-01-17 11:40:39 +0300

    Fixed bug, build
    Fixed ball teleport bug

--- Cube_Maze/Assets/Scripts/Gameplay/BallTeleport.cs
+++ Cube_Maze/Assets/Scripts/Gameplay/BallTeleport.cs
@@ -4,7 +4,16 @@ namespace Gameplay
 {
     public class BallTeleport : MonoBehaviour
     {
-        public void TeleportTo(Transform target) => transform.position = target.position;
+        private CharacterController _characterController;
+
+        private void Start() => _characterController = GetComponent<CharacterController>();
+
+        public void TeleportTo(Transform target)
+        {
+            _characterController.enabled = false;
+            transform.position = target.position;
+            _characterController.enabled = true;
+        }
     }
 }
 
